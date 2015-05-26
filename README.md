59969b0ed513f9b332407a0ca285c64af6145377
surajdazz <surajdazz@live.com>
2015-05-27 00:32:37 +0530

    Test 	new file:   .gitignore 	new file:   Android.mk 	new file:   AndroidBoard.mk 	new file:   BoardConfig.mk 	new file:   MODULE_LICENSE_APACHE2 	new file:   README.md 	new file:   audio/audio_platform_info.xml 	new file:   audio/audio_policy.conf 	new file:   audio/mixer_paths.xml 	new file:   bluetooth/bdroid_buildcfg.h 	new file:   camera/Android.mk 	new file:   camera/CameraWrapper.cpp 	new file:   device-proprietary-files.txt 	new file:   deviceextra/Android.mk 	new file:   deviceextra/AndroidManifest.xml 	new file:   deviceextra/res/mipmap-xhdpi/ic_launcher_settings.png 	new file:   deviceextra/res/mipmap-xxhdpi/ic_launcher_settings.png 	new file:   deviceextra/res/mipmap-xxxhdpi/ic_launcher_settings.png 	new file:   deviceextra/res/values-de/strings.xml 	new file:   deviceextra/res/values-it/strings.xml 	new file:   deviceextra/res/values-nl/strings.xml 	new file:   deviceextra/res/values-pt-rBR/strings.xml 	new file:   deviceextra/res/values-pt-rPT/strings.xml 	new file:   deviceextra/res/values-zh-rCN/strings.xml 	new file:   deviceextra/res/values/strings.xml 	new file:   deviceextra/res/values/styles.xml 	new file:   deviceextra/res/xml/device_settings.xml 	new file:   deviceextra/src/org/namelessrom/device/extra/ColorEnhancement.java 	new file:   deviceextra/src/org/namelessrom/device/extra/FileUtils.java 	new file:   deviceextra/src/org/namelessrom/device/extra/SettingsActivity.java 	new file:   deviceextra/src/org/namelessrom/device/extra/receivers/ControlReceiver.java 	new file:   deviceextra/src/org/namelessrom/device/extra/receivers/Startup.java 	new file:   extract-files.sh 	new file:   find7.mk 	new file:   init/Android.mk 	new file:   init/init_find7.c 	new file:   mkbootimg.mk 	new file:   nfc/Android.mk 	new file:   nfc/nfc_hw.c 	new file:   overlay/frameworks/base/core/res/res/values/config.xml 	new file:   overlay/frameworks/base/core/res/res/xml/power_profile_find7a.xml 	new file:   overlay/frameworks/base/core/res/res/xml/power_profile_find7s.xml 	new file:   overlay/frameworks/base/core/res/res/xml/storage_list_emu.xml 	new file:   overlay/frameworks/base/core/res/res/xml/storage_list_std.xml 	new file:   overlay/frameworks/base/packages/SystemUI/res/values/config.xml 	new file:   overlay/packages/apps/Dialer/res/values/config.xml 	new file:   rootdir/Android.mk 	new file:   rootdir/etc/fstab.qcom 	new file:   rootdir/etc/init.fs.rc 	new file:   rootdir/etc/init.qcom.rc 	new file:   rootdir/etc/init.qcom.usb.rc 	new file:   rootdir/etc/lvm/fstab.qcom 	new file:   rootdir/etc/lvm/lvm 	new file:   rootdir/etc/lvm/lvm.conf 	new file:   rootdir/etc/storage_earlyinit.sh 	new file:   rootdir/etc/storage_init.sh 	new file:   rootdir/etc/unified/fstab.qcom 	new file:   rootdir/etc/unified/init.fs.rc 	new file:   rootdir/recovery/init.recovery.target.rc 	new file:   rootdir/recovery/twrp.fstab.lvm 	new file:   rootdir/recovery/twrp.fstab.std 	new file:   rootdir/recovery/twrp.fstab.ufd 	new file:   sepolicy/device.te 	new file:   sepolicy/file_contexts 	new file:   sepolicy/init.te 	new file:   sepolicy/init_shell.te 	new file:   sepolicy/lvm.te 	new file:   sepolicy/vold.te 	new file:   setup-makefiles.sh 	new file:   setupwizard/res/layout/setup_find7_services.xml 	new file:   setupwizard/res/values-de/custom_strings.xml 	new file:   setupwizard/res/values-it/custom_strings.xml 	new file:   setupwizard/res/values-nl/custom_strings.xml 	new file:   setupwizard/res/values-pt-rBR/custom_strings.xml 	new file:   setupwizard/res/values-pt-rPT/custom_strings.xml 	new file:   setupwizard/res/values-zh-rCN/custom_strings.xml 	new file:   setupwizard/res/values/custom_strings.xml 	new file:   setupwizard/src/org/namelessrom/setupwizard/device/DeviceSpecificPages.java 	new file:   setupwizard/src/org/namelessrom/setupwizard/device/Find7SpecificPage.java 	new file:   system.prop

---
+++ README.md
@@ -0,0 +1,6 @@
+Device tree for OPPO Find7 universal (Find7a, Find7s, Stock layout, Unified layout, LVM)
+
+Copyright 2014 - The CyanogenMod Project
+
+Copyright 2014 - The Nameless Project
+
---
+++ rootdir/Android.mk
@@ -0,0 +1,92 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# Device init files
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom.std
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.fs.rc.std
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.fs.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom.ufd
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/unified/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.fs.rc.emu
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/unified/init.fs.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.usb.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := storage_earlyinit.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := etc/storage_earlyinit.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := storage_init.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := etc/storage_init.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom.lvm
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/lvm/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := lvm
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := etc/lvm/lvm
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/sbin
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := lvm.conf
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/lvm/lvm.conf
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/lvm/etc
+include $(BUILD_PREBUILT)
